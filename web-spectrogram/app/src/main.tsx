29021304c2425dd8ca839955024a6d3b02965f3f
Kian Ostad <c@kianostad.com>
2025-08-17 02:23:54 +0200

    test: cover service worker build and registration (#217)

--- web-spectrogram/app/src/main.tsx
+++ web-spectrogram/app/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import { registerServiceWorker } from "./register-sw";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -9,8 +10,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   </React.StrictMode>,
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
+registerServiceWorker();
