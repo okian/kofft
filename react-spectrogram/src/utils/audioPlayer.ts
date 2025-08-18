c41dcef57c06cc79ad5fa34c4de8b5f4083b93d5
Kian Ostad <c@kianostad.com>
2025-08-19 01:36:10 +0200

    fix(audio): resume from pause position

--- react-spectrogram/src/utils/audioPlayer.ts
+++ react-spectrogram/src/utils/audioPlayer.ts
@@ -185,7 +185,7 @@ class AudioPlayerEngine {
   }
 
   // Play a track
-  async playTrack(track: any): Promise<void> {
+  async playTrack(track: any, startAt = 0): Promise<void> {
     const requestId = ++this.playRequestId
     try {
       const context = await this.initAudioContext()
@@ -217,9 +217,9 @@ class AudioPlayerEngine {
         this.handleTrackEnded()
       }
 
-      // Start playback
-      this.source.start(0)
-      this.startTime = context.currentTime
+      // Start playback at the requested offset
+      this.source.start(0, startAt)
+      this.startTime = context.currentTime - startAt
 
       // Start time update loop
       this.updateTime()
@@ -256,7 +256,8 @@ class AudioPlayerEngine {
   // Resume playback
   resumePlayback(): void {
     if (this.isPaused && this.currentBuffer && this.currentTrack) {
-      this.playTrack(this.currentTrack)
+      const offset = this.pausedTime
+      this.playTrack(this.currentTrack, offset)
     }
   }
 
